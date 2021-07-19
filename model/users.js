fd4e1e64a60209558ae458761ea109727b0804e2
shafe <alam.shafee@gmail.com>
2021-07-19 20:15:47 +0600

    encapsulating logic in mongoose model

--- model/users.js
+++ model/users.js
@@ -1,35 +1,43 @@
+const config = require("config");
+const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const Joi = require("joi");
 const passwordComplexity = require("joi-password-complexity");
 
-const User = mongoose.model(
-  "User",
-  new mongoose.Schema({
-    date: {
-      type: Date,
-      default: Date.now,
-    },
-    username: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 255,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      minlength: 5,
-      maxlength: 255,
-    },
-    password: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 1024,
-    },
-  })
-);
+const userSchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  username: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 5,
+    maxlength: 255,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024,
+  },
+});
+
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, config.get("jwtPrivateKey"));
+  return token;
+};
+
+
+const User = mongoose.model("User", userSchema);
 
 function validateUser(user) {
   const schema = Joi.object({
